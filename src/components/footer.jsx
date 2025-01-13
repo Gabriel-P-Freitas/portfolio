eb355823c34dc6c829041a2db8d2dfe4d76c2a9d
Gabriel-P-Freitas <f.pessoa@escolar.ifrn.edu.br>
2025-01-13 10:50:45 -0300

    ajustando logo

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
         <div className="container mx-auto content-footer gap-y-6 flex items-center justify-between relative">
           <p className="copyright-text flex-auto">Copyright © Gabriel Pessoa · 2024</p>
           <div className="content-mobile flex items-center justify-between ">
-            <img className="logo-footer absolute center-x" src="public/imgs/logo.svg" alt="" />
+            <img className="logo-footer absolute center-x" src="./imgs/logo.svg" alt="" />
             <ul className="flex gap-x-4 items-center justify-self-start">
               <li><a className="social-links w-max h-max" href="https://github.com/Gabriel-P-Freitas" target="_blank"><i className="text-4xl align-middle transition-ease-in fa-brands fa-github"></i></a></li>
               <li><a className="social-links" href="https://www.linkedin.com/in/gabriel-pessoa-27239427b/" target="_blank"><i className="text-4xl align-middle transition-ease-in rounded-full fa-brands fa-linkedin"></i></a></li>
